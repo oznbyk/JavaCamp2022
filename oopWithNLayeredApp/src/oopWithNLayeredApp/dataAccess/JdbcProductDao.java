82beabf0f2fa4b5649fd867ca8046329b7913b32
oznbyk <ozan.bayik@reateknoloji.com>
2022-10-05 23:45:27 +0300

    Day3 Polymorphism & Nlayer Arch.

---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
@@ -0,0 +1,13 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class JdbcProductDao implements ProductDao {
+
+	@Override
+	public void add(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+}
